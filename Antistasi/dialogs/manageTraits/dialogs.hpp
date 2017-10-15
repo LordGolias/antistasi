0c436101b79141baf50fde7cf2fd3d28e04289c8
Golias <lord.golias1@gmail.com>
2017-10-15 23:03:51 +0200

    Fixed traits.

--- Antistasi/dialogs/manageTraits/dialogs.hpp
+++ Antistasi/dialogs/manageTraits/dialogs.hpp
@@ -7,9 +7,12 @@ class AS_manageTraits
 	{
 AS_DIALOG(5,"Add expertise", "closeDialog 0;");
 
-LIST_L(0,1,0,4,"");
-READ(1,1,1,4,"Trait description");
+READ(0,1,-1,1,"Possible expertises");
+LIST_L(0,2,0,3,"[0] spawn AS_fnc_UI_manageTraits_select;");
+READ(1,1,-1,1,"Your expertises");
+LIST_L(1,2,10,3,"[10] spawn AS_fnc_UI_manageTraits_select;");
+READ(0,5,1,1,"Trait description");
 
-BTN_R(5,4,"Train yourself", "Add the selected trait to yourself", "[] call AS_fnc_UI_manageTraits_select;");
+BTN_R(5,4,"Train yourself", "Add the selected trait to yourself", "[] spawn AS_fnc_UI_manageTraits_train;");
 	};
 };
